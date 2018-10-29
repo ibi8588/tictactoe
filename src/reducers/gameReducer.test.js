5fbfec7a89534712f118da5fa0ff6b66b52d7a29
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-10-28 22:19:16 -0700

    added test for winning game

--- src/reducers/gameReducer.test.js
+++ src/reducers/gameReducer.test.js
@@ -17,3 +17,19 @@ it('Should add a symbol at given position and change turn', () => {
   expect(nextState.board[0]).toEqual([O, '', '']);
   expect(nextState.turn).toEqual(X);
 });
+
+it('Should set "won" symbol when a winning line is set', () => {
+  const state = {
+    board: {
+      0: [X,  O, ''],
+      1: ['', X, ''],
+      2: [O, '', '']
+    },
+    won: undefined,
+    wonLine: undefined,
+    draw: false,
+    turn: X
+  };
+  const nextState = gameReducer(state, {type: 'ADD_SYMBOL', symbol: X, row: 2, position: 2});
+  expect(nextState.won).toEqual(X);
+});
