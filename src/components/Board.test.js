b20ff6bf86488a76aa206be2256bf4774545821a
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-10-28 22:01:11 -0700

    test to see if board renders

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -5,3 +5,18 @@ import BlankSymbol from './BlankSymbol';
 import XSymbol from './XSymbol';
 import OSymbol from './OSymbol';
 import { PureBoard as Board } from './Board';
+
+const board = {
+    0: [X, O, ''],
+    1: ['', X, O],
+    2: [X, X, O]
+  };
+
+  it('Should render Board with symbols', () => {
+  const startAgain = jest.fn();
+  const addSymbol = jest.fn();
+  const wrapper = shallow(<Board board={board} startAgain={startAgain} addSymbol={addSymbol} draw={false} turn={X} />);
+  expect(wrapper.find(XSymbol).length).toBe(4);
+  expect(wrapper.find(OSymbol).length).toBe(3);
+  expect(wrapper.find(BlankSymbol).length).toBe(2);
+});
