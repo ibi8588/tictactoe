6588cebda9ea80ee4cd68e014989ac988e561a6e
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-10-28 21:17:14 -0700

    logic fo columns

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -1,3 +1,7 @@
 const countInRow = (symbol, row) => row.filter(el => el === symbol).length;
 const hasWonInRow = (symbol, row) => countInRow(symbol, row) === 3;
 export const hasThreatInRow = (symbol, row) => countInRow(symbol, row) === 2;
+
+const countInColumn = (symbol, colNumber, ...rows) => rows.map(row => row[colNumber]).filter(el => el === symbol).length;
+const hasWonInColumn = (symbol, colNumber, ...rows) => countInColumn(symbol, colNumber, ...rows) === 3;
+export const hasThreatInColumn = (symbol, colNumber, ...rows) => countInColumn(symbol, colNumber, ...rows) === 2;
