d956211e50d1aae5cb3f41566a2df2ad2b46c676
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-10-28 21:59:21 -0700

    test for the board

---
+++ src/components/Board.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { X, O } from '../symbols/symbols';
+import BlankSymbol from './BlankSymbol';
+import XSymbol from './XSymbol';
+import OSymbol from './OSymbol';
+import { PureBoard as Board } from './Board';
