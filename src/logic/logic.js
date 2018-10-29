7b6a63064f27c35169b23f0c8ea0348ff5c02beb
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-10-28 21:15:47 -0700

    added logic

---
+++ src/logic/logic.js
@@ -0,0 +1,3 @@
+const countInRow = (symbol, row) => row.filter(el => el === symbol).length;
+const hasWonInRow = (symbol, row) => countInRow(symbol, row) === 3;
+export const hasThreatInRow = (symbol, row) => countInRow(symbol, row) === 2;
