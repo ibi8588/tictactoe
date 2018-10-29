dda8cf090e9be5914d6fcc3f9a3fc61de57fe114
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-10-28 22:06:17 -0700

    added test for result on current turn

--- src/components/Result.test.js
+++ src/components/Result.test.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { PureResult as Result } from './Result';
+
+it('Should render the Result component with message about current turn', () => {
+  const wrapper = shallow(<Result turn={'o'} draw={false} />);
+  expect(wrapper.find('p').node.props.children).toEqual('It\'s O\'s turn.');
+});
