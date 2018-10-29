a23c307c4ac67da4a6b4c9eae64c28122d7492d3
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-10-28 22:06:45 -0700

    win result test

--- src/components/Result.test.js
+++ src/components/Result.test.js
@@ -6,3 +6,8 @@ it('Should render the Result component with message about current turn', () => {
   const wrapper = shallow(<Result turn={'o'} draw={false} />);
   expect(wrapper.find('p').node.props.children).toEqual('It\'s O\'s turn.');
 });
+
+it('Should render the Result component with message about winning symbol', () => {
+  const wrapper = shallow(<Result won={'x'} draw={false} turn={'x'} />);
+  expect(wrapper.find('p').node.props.children).toEqual('Yay! X won!');
+});
